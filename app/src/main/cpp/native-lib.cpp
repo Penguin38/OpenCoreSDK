ae0d3c7fb51c2fb0d9c80f78b185b10dfa0603cf
Penguin <chenguanyou9338@gmail.com>
2023-06-06 01:20:38 +0800

    remove app
    Signed-off-by: Penguin <chenguanyou9338@gmail.com>

--- app/src/main/cpp/native-lib.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_penguin_opencore_tester_MainActivity_nativeCrashJNI(JNIEnv *env, jobject thiz) {
-    int *p = nullptr;
-    *p = 1;
-}
