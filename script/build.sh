447dace0fe9db50c212e46cf11b9d0af0ddb49b7
Penguin. <chenguanyou9338@gmail.com>
2025-01-13 21:07:53 +0800

    add local build script
    Signed-off-by: Penguin. <chenguanyou9338@gmail.com>

---
+++ script/build.sh
@@ -0,0 +1,8 @@
+if [ -z $JAVA_HOME ];then
+    echo "JAVA_HOME is not set"
+    echo "Example:"
+    echo "    export JAVA_HOME=JDK_DIR"
+    echo "    ./script/build.sh"
+    exit
+fi
+./gradlew clean -Pgroup=com.github.Penguin38 -Pversion=opencore-1.4.8 -xtest -xlint assemble publishToMavenLocal
